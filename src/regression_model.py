4b86cfdbf3cb36b6feb912a792b3cf176028c28d
Nincey Infanta <164032979+Ninceythomas@users.noreply.github.com>
2026-02-21 02:02:52 +0530

    Update import path for datacleaning module

--- src/regression_model.py
+++ src/regression_model.py
@@ -2,7 +2,7 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score, mean_squared_error
-from datacleaning import merge_data, encode_features
+from src.datacleaning import merge_data, encode_features
 import numpy as np
 
 
